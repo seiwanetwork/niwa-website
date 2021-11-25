761f04812a4adebdae5438e760da3c302953a822
Jared <KeybladeSpyMaster@users.noreply.github.com>
2021-11-25 00:27:20 -0700

    Update join.php

--- join.php
+++ join.php
@@ -6,20 +6,23 @@ include('_header.php');
 <div class="main">
     <h1>Join SEIWA</h1>
 
-    <p>Intrested in joining the Square Enix Independent Wiki Alliance? Check out these following guidelines to see if you should apply for membership:</p>
+    <p>Intrested in joining the Square Enix Independent Wiki Alliance? Check out the following guidelines:</p>
 
     <ol>
-        
-    <li>Wikis should be centered on a franchise that Square Enix has been involved in. This could include media (such as games or manga) developed by Square Enix, its past entities (Squaresoft, Enix), or its subsidiaries.</li>
+    <li>Wikis must be centered around a franchise that Square Enix was involved in, in some way, shape or form. This includes, but is not limited to:</li>
+        <ol>
+            <li>Media developed by Square Enix (such as a game), Squaresoft, Enix Software or a subsidiary thereof.</li>
+            <li>Media published by Square Enix (such as a manga), Squaresoft, Enix Software or a subsidiary thereof.</li>
+        </ol>
     <li>Wikis should have at least 100 articles, of which at least 25 should not be considered stubs.</li>
-    <li>Wikis must have some form of activity in the last 90 days (~3 months). This can include</li>
+    <li>Wikis must have some form of activity in the last 90 days (~3 months). This can include:</li>
         <ol>
             <li>Edits to improve the quality of a page (templates, articles, project, etc.)</li>
             <li>Creation of new pages (files, policies, subpages, categories, etc.)</li>
             <li>Discussions</li>
             <li>Template documentation</li>
         </ol>
-    <li>Wikis should have policies in place to ensure quality content, prevent vandalism, etc. and an active staff to enforce them.</li>
+    <li>Wikis need policies in place to ensure Quality Content, Anti-vandal measures, etc. and a staff to enforce them</li>
     <li>Wikis need to maintain independence in such a way that their community as a whole can decide its internal and external policies.</li>
         <ol>
             <li>This includes the ability to choose to shut itself down, change hosts, etc.</li>
@@ -28,7 +31,7 @@ include('_header.php');
 
     <p>Once you apply, the community will discuss and vote internally to decide on membership.</p>
 
-    <p>Please note that these guidelines are subject to change without notice. If you are interested in joining or affiliating with SEIWA, please <a href="/contact">contact us</a>.</p>
+    <p>Please note that these guidelines are subject to change without notice. If you are interested in joining or affiliating with SEIWA, please <a href="/contact">contact us</a>!</p>
 </div>
 
 <?php
