337dc9eeec3029de4b5526a7e71b74fd2a742337
Jared <KeybladeSpyMaster@users.noreply.github.com>
2021-11-28 16:20:25 -0700

    Update _sidebar.php

--- _sidebar.php
+++ _sidebar.php
@@ -10,5 +10,17 @@
             sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts">
     </iframe>
   </div>
+  <div class="box" id="box-facebook">
+    <div class="title">Facebook</div>
+    <iframe 
+            src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fseiwanetwork&tabs&width=340&height=130&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" 
+            width="340" 
+            height="130" 
+            style="border:none;overflow:hidden" 
+            scrolling="no" 
+            frameborder="0" 
+            allowfullscreen="true" 
+            allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+  </div>
 </div>
 <p style="clear: both;"></p>
