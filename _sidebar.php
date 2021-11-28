fc1537f12f20ab7cf8ff37d380dc857259ab798c
Jared <KeybladeSpyMaster@users.noreply.github.com>
2021-11-28 15:57:41 -0700

    Update _sidebar.php

--- _sidebar.php
+++ _sidebar.php
@@ -10,6 +10,14 @@
         frameborder="0"
         sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts">
       </iframe>
+    <iframe 
+            src="https://discord.com/widget?id=493490207010717716&theme=dark" 
+            width="350" 
+            height="500" 
+            allowtransparency="true" 
+            frameborder="0" 
+            sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts">
+    </iframe>
   </div>
 </div>
 <p style="clear: both;"></p>
