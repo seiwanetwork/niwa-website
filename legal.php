4420879fd9985cfd3d9f2cb5bd348c60a087ac71
Jared <KeybladeSpyMaster@users.noreply.github.com>
2022-05-02 18:03:15 -0600

    Update legal.php

--- legal.php
+++ legal.php
@@ -6,9 +6,9 @@ include('_header.php');
 <div class="main">
   <h1>Legal</h1>
 
-  <p>All original content found throughout this website is copyright &copy; Square Enix Independent Wiki Alliance (SEIWA) 2011-<?php echo (date(Y)); ?>.</p>
-  <p>SEIWA is in no way associated with Square Enix or its subsidiary companies. In places where copyrighted Square Enix content is presented we do so with fair terms of use, whereby it is not used commercially for gain and where it does not impede Square Enix's right to sell the depicted material.</p>
-  <p>Member wikis of SEIWA are not bound by a single license. Please refer to the individual wikis for licensing, copyright, and content duplication policies.</p>
+  <p>Original content &copy; Square Enix Independent Wiki Alliance <?php echo (date(Y)); ?>.</p>
+  <p>The Square Enix Independent Wiki Alliance is not affiliated with Square Enix, its studios or subsidiaries, affiliates, divisions, or employees. Official Square Enix content &copy; Square Enix and used in accordance with fair use principles and guidelines.</p>
+  <p>SEIWA member wikis are not bound by a single license. Please refer to the individual wikis for licensing, copyright, and content use policies.</p>
 </div>
 
 <?php
